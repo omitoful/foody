1ec1e9201c13689a3d146305e41838fc02b6c156
“kevin.chen” <kevin.chen@medicrowd.com.tw>
2023-12-17 18:00:47 +0800

    header and menu items for homepage

---
+++ src/components/layout/HomeMenu.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import MenuItem from "../menu/MenuItem";
+
+export default function HomeMenu() {
+  return (
+    <section className="">
+      <div className=" absolute left-0 right-0 w-full justify-start">
+        <div className="absolute left-0 -top-[70px] text-left -z-10">
+          <Image src="/sallad1.png" width={109} height={189} alt="sallad" />
+        </div>
+        <div className="absolute -top-[100px] right-0 text-right -z-10">
+          <Image src="/sallad2.png" width={107} height={195} alt="sallad" />
+        </div>
+      </div>
+      <div className="text-center mb-4">
+        <h3 className="uppercase text-gray-600 font-semibold leading-3">
+          Check out
+        </h3>
+        <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+      </div>
+    </section>
+  );
+}
