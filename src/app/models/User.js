c3d97cfb482a6bb87618b4049c7e4610b9ed761e
“kevin.chen” <kevin.chen@medicrowd.com.tw>
2023-12-18 01:14:28 +0800

    connect to mongodb & install bcrypt & setup register

---
+++ src/app/models/User.js
@@ -0,0 +1,28 @@
+import { Schema, model, models } from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: {
+      type: String,
+      required: true,
+      validate: (pass) => {
+        if (!pass?.length || pass.length < 5) {
+          new Error("password must be at least 5 characters");
+          return false;
+        }
+        pass;
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.post("validate", function (user) {
+  const pass = user.password;
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(pass, salt);
+});
+
+export const User = models?.User || model("User", UserSchema);
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,13 +1,74 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    setCreatingUser(true);
+    setUserCreated(false);
+    setError(false);
+
+    const response = await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
+
+    setCreatingUser(false);
+  }
+
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
-      <form className="block max-w-xs mx-auto">
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <button type="submit">Register</button>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.
+          <br />
+          Now you can{" "}
+          <Link className="underline" href="/login">
+            Login &raquo;
+          </Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          Error!!!
+          <br />
+          Please try again later.
+        </div>
+      )}
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          disabled={creatingUser}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          disabled={creatingUser}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="text-center my-4 text-gray-500">
           or login with provider
         </div>
